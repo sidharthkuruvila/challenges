60413999368fe0f98c004b3d11bd2e6c6b2c83be
Sidharth Kuruvila <sidharth@canva.com>
2024-12-04 21:34:08 +1100

    Initial commit for day 05 in rust

---
+++ adventofcode/2024/rust/day05/src/main.rs
@@ -0,0 +1,11 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn main() {
+    let path = Path::new("../../input/day05/small-input.txt");
+    let mut file = File::open(path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    println!("Part 1: {}", contents);
+}
